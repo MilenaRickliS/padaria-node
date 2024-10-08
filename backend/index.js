38a3d8c2ff476956bfc4a95b7042dc80a3d6b303
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2024-10-07 23:09:31 -0300

    cc

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const server = express()
 const pao = require('./src/data/pao.json')
 
+const port = process.env.PORT || 8080;
+
 server.get('/pao', (req, res) =>{
     return res.json(pao)
 })
 
-server.listen(8080, () => {
+server.listen(port, () => {
     console.log('servidor está funcionando ...')
 })
