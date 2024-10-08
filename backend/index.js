97f89672051afaf2b0af64e28240aaaf61ea2cca
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2024-10-07 23:19:44 -0300

    cc

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const server = express()
 const pao = require('./src/data/pao.json')
 
-const port = process.env.PORT || 8080;
+const port = 8000;
 
 server.get('/pao', (req, res) =>{
     return res.json(pao)
